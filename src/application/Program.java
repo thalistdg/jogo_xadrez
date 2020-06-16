6f21924523af1874dbf18af355411aabbdef8a9b
Thalis Duarte <thalis_galeno@hotmail.com.br>
2020-06-16 17:39:15 -0300

    created class position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
- 
+		
+		Position pos = new Position(2, 3);
+		System.out.println(pos);
 
 	}
 
---
+++ src/boardgame/Position.java
@@ -0,0 +1,32 @@
+package boardgame;
+
+public class Position {
+	private Integer row;
+	private Integer column;
+	
+	public Position() {}
+
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+}
