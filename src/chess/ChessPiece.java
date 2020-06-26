333a0a44b74d38710c8f8f26106dbae13d59f141
Thalis Duarte <thalis_galeno@hotmail.com.br>
2020-06-26 18:09:16 -0300

    check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -20,6 +20,10 @@ public abstract class ChessPiece extends Piece {
 	public Integer getMoveCount() {
 		return moveCount;
 	}
+	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
 
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
